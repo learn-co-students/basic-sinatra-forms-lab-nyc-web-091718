89f7ce67eabc360f17b17043a9f7941a3dc09bbc
jtang93 <tang.jonathan93@gmail.com>
2018-10-10 18:17:23 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,18 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
 
+    erb :newteam
+  end
+
+  post '/team' do
+    @params = params
+    erb :team
+    # redirect '/team'
+  end
+
+  # get '/team' do
+  #   erb :team
+  # end
 end
